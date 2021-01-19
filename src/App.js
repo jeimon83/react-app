0355e6a7956c25a21761d1e0ee79305995a99553
Jaime Garcia Mendez <jaime@oyga.me>
2021-01-19 15:15:27 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import tasks from './samples/tasks.json';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-
-
 
 class App extends Component {
-
-  state = {
-    tasks: tasks
-  }
-
   render() {
-   return <div>
-        { 
-          this.state.tasks.map(t => <h1>{t.title} {t.description}</h1>) }
-      </div>   
+   return <div>Coder House - Curso de React JS</div>   
   }
 }
 
