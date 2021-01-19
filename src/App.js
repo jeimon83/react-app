6d8049b8881f6a7511fd29082ff9abb24991b73e
Jaime Garcia Mendez <jaime@oyga.me>
2021-01-19 15:04:39 -0300

    curso youtube

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import tasks from './samples/tasks.json';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  state = {
+    tasks: tasks
+  }
+
+  render() {
+   return <div>
+        { 
+          this.state.tasks.map(t => <h1>{t.title} {t.description}</h1>) }
+      </div>   
+  }
 }
 
 export default App;
