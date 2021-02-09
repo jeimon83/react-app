4a7f5c05afdbf8f15c83777913ebfc788f6c81be
Jaime Garcia Mendez <jaime@oyga.me>
2021-02-08 21:46:51 -0300

    image url

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -6,6 +6,7 @@ const Item = ({product}) => {
     <div className="card">
       <h3 className="title">{product.title}</h3>
       <h4 className="price">Precio: ${product.price}</h4>
+      <img alt="product" src={product.img}></img>
       <ItemCount stock={product.inventory} initial={1} />      
     </div>
   )
--- src/mocks/productList.js
+++ src/mocks/productList.js
@@ -1,21 +1,21 @@
 const productList = [
   { id: 1, title: "Adidas Precision Pro 2019",
     description: "Paleta de Padel Adidas Precision Pro 2019",
-    img: "./adidas-precision-2019.png", price: 22000, inventory: 3
+    img: "images/adidas-precision-2019.png", price: 22000, inventory: 3
   },
   { 
-    id: 2, title: "Head Evolution Bela 2018",
-    description: "Paleta de Padel Head Evolution Bela 2018",
-    img: "./head-evolution-bela.png", price: 18000, inventory: 5
+    id: 2, title: "Varlion Avant 2",
+    description: "Paleta de Padel Varlion Avant 2",
+    img: "images/varlion-avant2.png", price: 18000, inventory: 5
   },
   { id: 3, title: "Nox ML10 Pro Cup",
     description: "Paleta de Padel Nox ML10 Pro Cup",
-    img: "./nox-ml10-pro-cup.png" , price: 24000, inventory: 2
+    img: "images/nox-ml10-pro-cup.png" , price: 24000, inventory: 2
   },
   { 
     id: 4, title: "Bullpadel Heat Eva 2020",
     description: "Paleta de Padel Bullpadel Heat Eva 2020",
-    img: "./bullpadel-heat-eva.png", price: 25000, inventory: 4
+    img: "images/bullpadel-heat-eva.png", price: 25000, inventory: 4
   }
 ]
 
