21a4a6fd6f41f1017d67df8ccbddf25c04b8fae5
Jaime Garcia Mendez <jaime@oyga.me>
2021-02-08 21:52:03 -0300

    styles for images

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -2,11 +2,16 @@ import ItemCount from '../itemcount/ItemCount'
 import './style.css'
 
 const Item = ({product}) => {
+
+  var style= {
+    width: 200
+  };
+
   return (
     <div className="card">
       <h3 className="title">{product.title}</h3>
       <h4 className="price">Precio: ${product.price}</h4>
-      <img alt="product" src={product.img}></img>
+      <div><img alt="product" src={product.img} style={style}></img></div>
       <ItemCount stock={product.inventory} initial={1} />      
     </div>
   )
