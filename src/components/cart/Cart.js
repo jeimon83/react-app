bb5391a8938c0f2d9261c7f37a978ba05c8a95d7
Jaime Garcia Mendez <jaime@oyga.me>
2021-03-05 17:35:25 -0300

    creates order in firebase

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { useCartContext } from "../../context/CartContext.js"
 import Button from '@material-ui/core/Button';
+import { getFirestore } from '../../firebase'
 import './style.css'
 
 const Cart = () => {
@@ -12,6 +13,10 @@ const Cart = () => {
     totalPrice,
     totalItems
    } = useCartContext();
+
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [phone, setPhone] = useState('')
   
   const addOne = item => {
     if (item.stock === item.count) {
@@ -25,6 +30,28 @@ const Cart = () => {
 
   const total_price = () => totalPrice()
   const total_items = () => totalItems()
+
+  const finishBuy = () => {
+
+    let newDate = new Date()
+    let newOrder = { 
+      buyer: {
+        name: name,
+        email: email,
+        phone: phone
+      },
+      items: 
+        [...list],
+        date: newDate,
+        total: total_price()
+    }
+
+    const fsDB = getFirestore()
+    const orderCollection = fsDB.collection('orders')
+    orderCollection.add(newOrder).then((value) => {
+      console.log(value.id) 
+    })
+  }
 
   return (
     <>
@@ -52,6 +79,18 @@ const Cart = () => {
           <div>Total ${total_price()}</div>
           :
           <div>The cart is empty</div>
+        }
+      </div>
+      <div>
+        <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" placeholder="Phone" onChange={(e) => setPhone(e.target.value)} />
+      </div>
+      <div className="info">
+      {(total_items() > 0) ?
+          <button onClick={() => finishBuy()}>Finish Buy</button>
+          :
+          null
         }
       </div>
     </>
